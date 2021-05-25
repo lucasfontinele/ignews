fe736232a7b239e19871925829ae0adc1e065166
Lucas Fontinele <lfontinele07@gmail.com>
2021-05-25 09:45:45 -0300

    Implements application global style and header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello&nbsp;
-      <span>
-        World
-      </span>
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello&nbsp;
+        <span>
+          World
+        </span>
+      </h1>
+    </>
   );
 }
