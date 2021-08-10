8bdf0809fa49c0a3ac48e2ab07850497e284fbe9
Lucas Fontinele <lfontinele07@gmail.com>
2021-08-10 19:36:45 -0300

    Implements application redirect to stripe checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { signIn, useSession } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 type SubscribeButtonProps = {
@@ -5,10 +11,39 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
+      disabled={loading}
     >
       Subscribe now
     </button>
